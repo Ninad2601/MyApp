1e170ffa8d7ba50bcbce9fc2685bb0e9b720b2ae
Ninad <padhye.ninad@gmail.com>
2022-11-04 10:44:16 +0530

    Add project files.

---
+++ MyAppWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyAppWeb.Models;
+
+namespace MyAppWeb.Data
+{
+    /*This Class is used for registring the ConnectionString In appsetting*/
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
+        {
+
+        }
+        public DbSet<Category>Categories { get; set; }
+    }
+}
